b1d981def5642616befe697137d6bed4f827be6f
link237 <56715940+link237@users.noreply.github.com>
2025-09-04 20:04:18 -0700

    Bypass all _next routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -135,5 +135,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|api/auth/session).*)'],
+  matcher: ['/((?!_next|api|favicon.ico).*)'],
 };
